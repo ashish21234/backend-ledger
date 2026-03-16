aba6454866533f8343587840ce554d1100f44d97
ashish21234 <ashishranjan21234@gmail.com>
2026-03-17 01:23:18 +0530

    Added functionality of system initial funds, transaction management and logout feature

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
 const emailService = require("../services/email.service")
-
+const tokenBlackListModel = require("../models/blackList.model")
 /**
  * - user register controller
  * -POST /api/auth/register
@@ -75,7 +75,29 @@ async function userLoginController(req, res) {
         token
     })
 }
+
+/**
+ *  -USER Logout Controller
+ *  -POST /api/auth/logout
+ */
+
+async function userLogoutController(req, res) {
+    const token = req.cookies.token || req.headers.authorization?.split("")[1]
+    if (!token) {
+        return res.status(400).json({
+            message: "User logged out successfully"
+        })
+    }
+    res.clearCookie("token")
+    await tokenBlackListModel.create({
+        token: token
+    })
+    res.status(200).json({
+        message: "User logged out successfully"
+    })
+}
 module.exports = {
     userRegisterController,
-    userLoginController
+    userLoginController,
+    userLogoutController
 }
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -9,4 +9,18 @@ const accountController = require("../controllers/account.controller")
 */
 router.post("/", authMiddleware.authMiddleware, accountController.createAccountController)
 
-module.exports = router 
+/**
+ * -GET /api/accounts/
+ * -GET all accounts of the logged-in user
+ * -Protected Route
+ */
+router.get("/", authMiddleware.authMiddleware, accountController.getUserAccountsController)
+
+/**
+ * -GET /api/accounts/balance/:accountId
+ * -Get balance of a specific account
+ * -Protected Route
+ */
+router.get("/balance/:accountId", authMiddleware.authMiddleware, accountController.getAccountBalanceController)
+
+module.exports = router
